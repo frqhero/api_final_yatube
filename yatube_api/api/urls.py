d4e908719c90a72aa05e7c0a72c1466821a5c10c
frqhero <frqhero@gmail.com>
2022-09-16 17:22:43 +0300

    get posts works

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from rest_framework import routers
+from .views import PostViewSet
+from django.urls import include
+
+router = routers.SimpleRouter()
+router.register('posts', PostViewSet)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,14 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from posts.models import Post
+from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
