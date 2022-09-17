8b24dec74c9f975dda83742e83e84348d9f70f10
lenovo ubuntu <frqhero@gmail.com>
2022-09-17 11:01:00 +0300

    group field added to post

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,13 +2,14 @@ from rest_framework import viewsets
 from posts.models import Post
 from .serializers import PostSerializer
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
