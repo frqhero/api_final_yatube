7d47a114b00277c34546d6a574b59eeeb6e5fe84
work pc <frqhero@gmail.com>
2022-09-19 12:20:17 +0300

    comments are done

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework import routers
-from .views import PostViewSet, GroupViewSet
+from .views import PostViewSet, GroupViewSet, CommentViewSet
 from django.urls import include
 
 router = routers.SimpleRouter()
 router.register('posts', PostViewSet)
 router.register('groups', GroupViewSet)
+router.register(r'^posts/(?P<pk_post>\d+)/comments', CommentViewSet,
+                basename='comment')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
-from posts.models import Post, Group
-from .serializers import PostSerializer, GroupSerializer
+from posts.models import Post, Group, Comment
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import LimitOffsetPagination
+from django.shortcuts import get_object_or_404
 
 
 
@@ -21,3 +22,17 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        pk_post = self.kwargs.get('pk_post')
+        post = get_object_or_404(Post, pk=pk_post)
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('pk_post'))
+        serializer.save(author=self.request.user, post=post)
